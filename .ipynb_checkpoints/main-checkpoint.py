581b9b9dec07c2401a6fc21a88013074ebe8cc88
sable <sabure0241@gmail.com>
2021-11-16 18:59:22 +0900

    1st commit

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,17 @@
+import json
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
+file = open('info.json','r')
+info = json.load(file)
+CHANNELACCESSTOKEN = info["CHANNELACCESSTOKEN"]
+line_bot_api = LineBotApi(CHANNELACCESSTOKEN)
+
+def main():
+    USERID = info["USERID"]
+    messages = TextSendMessage(text="おはよう")
+    line_bot_api.push_message(USERID,messages=messages)
+    
+if __name__=="__main__":
+    main()
+
