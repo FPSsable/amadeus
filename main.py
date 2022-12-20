36a25efb46a97b3a31b68894d13d916332c958e8
FPSsable <93975994+FPSsable@users.noreply.github.com>
2022-12-20 14:00:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ line_bot_api = LineBotApi(CHANNELACCESSTOKEN)
 
 def main():
     USERID = info["USERID"]
-    l = ["おきなさい","いつまで寝てるのよ？","遅刻するよ",]
+    l = ["おきなさい","いつまで寝てるの？","遅刻するよ",]
     messages = TextSendMessage(text="おはよう。昨日はよく眠れた？")
     line_bot_api.push_message(USERID,messages=messages)
     time.sleep(600)
@@ -21,4 +21,3 @@ def main():
     
 if __name__=="__main__":
     main()
-
